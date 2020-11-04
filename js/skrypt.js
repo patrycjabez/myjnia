05f1690cdb360927384a98aef51f601c75de6222
patrycjabez <franixxx1994@gmail.com>
2020-11-04 15:54:02 +0100

    added variables

---
+++ js/skrypt.js
@@ -0,0 +1,8 @@
+let imie = document.getElementById("#imie");
+let mycie = document.getElementById("#mycie").checked;
+let suszenie = document.getElementById("#suszenie").checked;
+let woskowanie = document.getElementById("#woskowanie").checked;
+let odkurzanie = document.getElementById("#odkurzanie").checked;
+let rodzaj = document.getElementById("#rodzaj").value;
+let karta = document.getElementsByName("klient").selected;
+
